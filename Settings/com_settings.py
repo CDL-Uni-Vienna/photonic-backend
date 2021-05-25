311701f76a7e65065542027e2d4409e2b4f6f39a
Felix Zilk <zilk.felix@gmail.com>
2021-05-25 16:48:50 +0200

    change gitignore

---
+++ Settings/com_settings.py
@@ -0,0 +1,6 @@
+# sn = "1907988" # Power meter's serial number
+# sn = '201117311' # new usb powermeter
+com = 'COM3'
+# in Windows: Find COM port in device manager
+
+#pm = open_powermeter(sn,2)# 1: usb pm, other number, non-usb powermeter
---
+++ Settings/offset_settings.py
@@ -0,0 +1,5 @@
+# offset:
+# 73.59
+# 79.67 for test wp
+
+offset = [0, 0]
