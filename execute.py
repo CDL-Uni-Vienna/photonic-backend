4649ab8889cab87daf81d04fa0312af6f60e3d1c
CDL-Univie <77630497+CDL-Univie@users.noreply.github.com>
2021-05-25 20:36:52 +0200

    change execute.py

--- execute.py
+++ execute.py
@@ -8,14 +8,13 @@ def h_gate(qubit):
     """
     
 
-
 print("Request data in JSON:")
-data = rest.get(3)
+data = rest.get(4)
 print(data)
 print("-----")
 print(data["experiment"][0])
 
 
-for shot in range(0, data["shots"]):
-    for command in data["experiment"]:
-        if command["name"] == "H":
+# for shot in range(0, data["shots"]):
+#     for command in data["experiment"]:
+#         if command["name"] == "H":
