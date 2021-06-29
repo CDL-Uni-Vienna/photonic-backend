29f656859dfb5dc263436fcee86f3a519f8318e7
omatiz <omatiz@gmail.com>
2021-06-29 10:34:18 -0500

    trial of Census functionality

--- test_census.py
+++ test_census.py
@@ -1,14 +1,41 @@
 from ThorLabsMotors.RotationMount import move_abs, open_serial, get_info
-import Settings.offset_settings as offset_settings
+from Settings.measurement_settings import setupDic
 import Settings.com_settings as com_settings
 
-bus = open_serial(com_settings.com1,  timeout=0.5)
+devices_known = []
 
-get_info(bus, 0)
+devices_unkno = []
+
+for comPort in com_settings.com_list:
+
+    bus = open_serial(comPort,  timeout=0.3)
+
+    for motorAddress in com_settings.address_list:
+
+        device_sn = get_info(bus, motorAddress)
+
+        element = setupDic.get[device_sn]
+        if element is None:
+            devices_unkno.append(element)
+            print(''.join(['Warning :: Element ', element, ' is not present in the setup dictionary (setupDic)' ]))
+        else:
+            devices_known.append(element)
+
+    bus.close()
+
+print(devices_known)
+
+devices_known
+
+
+
+
+
+
 
 # move_abs(bus, 0, 45)
 
-bus.close()
+
 
 '''
 b'0IN0E1140050920201501016800023000\r\n'
