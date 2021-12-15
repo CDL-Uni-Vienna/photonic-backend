ba1c7baffd8e5d920274f84a8b4015799dffdc01
CDL-Univie <77630497+CDL-Univie@users.noreply.github.com>
2021-12-15 18:11:07 +0100

    add rest_script.py

--- RestCalls/rest_script.py
+++ RestCalls/rest_script.py
@@ -1,1 +1,18 @@
-import rest_calls
+import requests
+import json
+
+url = "https://quco.exp.univie.ac.at/api/login"
+
+payload = json.dumps({
+    "username": "test@test.at",
+    "password": "test"
+})
+headers = {
+    'Content-Type': 'application/json',
+    'Cookie': 'csrftoken=j3G9nCYFFI9TZJIFbygCkxdLfB9TzwKYXqa8F2dh7LJDzmLGbH4DM0ngA1vARG7w; sessionid=0qgcg0qct8i1sojuxzwuwsgn4miyxxyb'
+}
+
+response = requests.request(
+    "POST", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
