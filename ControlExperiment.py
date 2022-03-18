5980dfb150e1fd4235ced3227c680b75c49d9121
CDL-Univie <77630497+CDL-Univie@users.noreply.github.com>
2022-03-18 18:26:38 +0100

    Update for non L4 states. Added proper prints

--- ControlExperiment.py
+++ ControlExperiment.py
@@ -6,7 +6,9 @@ from ControlPlatesArray import PlatesArray
 
 
 class Experiment:
-    """"""
+    '''
+    Class for controlling all waveplates according to the experiment dictonary requirements.
+    '''
 
     def __init__(self, expeDic: dict):
         '''
@@ -16,12 +18,13 @@ class Experiment:
         ----------
         expeDic : Dictionary of the experiment
         '''
+        mssg = 'Experiment.init :: '
 
         self.circuitId = expeDic["circuitId"]
         self.circuitAngles = expeDic["ComputeSettings"]["qubitComputing"]["circuitAngles"]
         self.platesAnglesDic = {}
         self.encodedQubitMeasurements = expeDic["ComputeSettings"]["encodedQubitMeasurements"]
-        print('Initializing experiment...\n\tcircuitId: ' + str(self.circuitId))
+        print(mssg + 'Initializing experiment with circuitId: ' + str(self.circuitId))
 
         d = os.getcwd()
         #d = os.path.join(d, 'photonic-backend', 'CircuitLib','circuits4Dv004.json')
@@ -31,96 +34,134 @@ class Experiment:
             self.circuitLib = json.load(read_file)
             # Load the lib. TODO: remove position usage to find circuitLib entry
             self.circuitLib = self.circuitLib[self.circuitId-1]
+            print(mssg + "Circuit library (circuitLib) entry identified:")
+            print(self.circuitLib)
 
     def getPlatesAngles(self):
         '''
         gets the list of angles for each plate from the experiment conf.
         '''
-        print('Calculating plates angles...')
+        mssg = 'Experiment.getPlatesAngles :: '
+
+        print(mssg + 'Calculating plates angles...')
 
         # Cluster state preparation
         self.clusterStatePrepPaths = [0]
+
         prename = self.circuitLib["csp_preset_settings_name"]
+
+        print(mssg + 'Preparing ' + prename + ' state')
+
         if prename == "Linear Cluster":
             self.clusterStatePrep = [[22.5]]
-        elif prename == "Greenberger–Horne–Zeilinger":
+        # "Greenberger–Horne–Zeilinger":
+        elif prename == 'Greenbergerâ€“Horneâ€“Zeilinger':
             self.clusterStatePrep = [[0]]
         else:
             print('not recognizible csp_preset_settings_name')
 
         # Qubit computing angles
         if len(self.circuitAngles) > 0:
-            #print( [*map( lambda d: self.circuitLib['circuit_tag2path_dic'][d["circuitAngleName"]], self.circuitAngles)])
+
             self.circuitAnglesPaths = [
-                *map(lambda d: self.circuitLib['circuit_tag2path_dic'][d["circuitAngleName"]], self.circuitAngles)]
+                *map(lambda d: self.circuitLib['circuit_tag2path_dic'][d["circuitAngleName"]], self.circuitAngles)
+            ]
+            # print(mssg + "Optical paths to be used for rotations: " +
+            #      str(self.circuitAnglesPaths))
+
             self.circuitAngles = [
-                *map(lambda d: d["circuitAngleValue"], self.circuitAngles)]
+                *map(lambda d: d["circuitAngleValue"], self.circuitAngles)
+            ]
+            print(mssg + 'Calculating waveplates angles to implement encoded circuit ' +
+                  str(self.circuitId) + ' with rotation(s) parameters: ' + str(self.circuitAngles))
+
+            print(mssg + "Optical paths to be used for rotations: " +
+                  str(self.circuitAnglesPaths))
+
             if prename == "Linear Cluster":
                 self.circuitAngles = [
-                    *map(alphaToPlatesAngles, self.circuitAngles)]
-            elif prename == "Greenberger–Horne–Zeilinger":
+                    *map(alphaToPlatesAngles, self.circuitAngles)
+                ]
+                print(mssg + "Raw waveplates angles for rotation(s): " +
+                      str(self.circuitAngles))
+            # "Greenberger–Horne–Zeilinger":
+            elif prename == 'Greenbergerâ€“Horneâ€“Zeilinger':
                 self.circuitAngles = [
-                    *map(alphaToPlatesAngles, self.circuitAngles)]
+                    *map(alphaToPlatesAngles, self.circuitAngles)
+                ]
+                print(mssg + "Raw waveplates angles for rotation(s): " +
+                      str(self.circuitAngles))
         else:
+
             self.circuitAngles = []
 
         # Qubit measurements
         self.encodedQubitMeasurementsPaths = [
-            *map(lambda d: self.circuitLib['circuit_tag2path_dic'][str(d["encodedQubitIndex"])], self.encodedQubitMeasurements)]
-        self.encodedQubitMeasurements = [*map(lambda d: [
-            d["theta"], d["phi"]
-        ], self.encodedQubitMeasurements)]
+            *map(lambda d: self.circuitLib['circuit_tag2path_dic'][str(d["encodedQubitIndex"])], self.encodedQubitMeasurements)
+        ]
+        print(mssg + "Optical paths to be used for qubit measurements: " +
+              str(self.encodedQubitMeasurementsPaths))
+
+        self.encodedQubitMeasurements = [
+            *map(lambda d: [
+                d["theta"], d["phi"]
+            ], self.encodedQubitMeasurements)
+        ]
+        print(mssg + "Qubit measurements parameters theta, phi: " +
+              str(self.encodedQubitMeasurements))
+
         self.encodedQubitMeasurements = [
             *map(thphToPlatesAngles, self.encodedQubitMeasurements)]
+        print(mssg + "Qubit measurements waveplates angles: " +
+              str(self.encodedQubitMeasurements))
 
-        # Merging all angles
-        self.platesAngles = flatten(flatten([
-            self.clusterStatePrep,
-            self.circuitAngles,
-            self.encodedQubitMeasurements
-        ]))
-
-        self.platesAngles = [*map(lambda ang: ang % 360, self.platesAngles)]
-
-        print(self.platesAngles)
-
-        # Loadnig both path and angle info in platesAnglesDic
+        # Loading both path and angle info in platesAnglesDic
+        # Notice the Paths sufix at variables. It denotes a relation between the paths and angles variables (angles grouped by module)
         for num, pp in enumerate(self.clusterStatePrepPaths):
             self.platesAnglesDic[pp] = self.clusterStatePrep[num]
 
-        for num, pp in enumerate(self.circuitAnglesPaths):
-            self.platesAnglesDic[pp] = self.circuitAngles[num]
+        # circuitAnglesPaths contains the information of which free rotation parameter of the encoded circuit corresponds to which physical qubit path.
+        # If there are no rotations done using the one-way model, i.e. all physical qubits are encoded ones then circuitAnglesPahts = []
+        if len(self.circuitAngles) > 0:
+            for num, pp in enumerate(self.circuitAnglesPaths):
+                self.platesAnglesDic[pp] = self.circuitAngles[num]
 
         for num, pp in enumerate(self.encodedQubitMeasurementsPaths):
             self.platesAnglesDic[pp] = self.encodedQubitMeasurements[num]
 
+        dimension = self.circuitLib["csp_number_of_qubits"]
+        # If a new cluster state is added, an update is needed here (below)
+        if dimension == 3:
+            self.platesAnglesDic[4] = [0.0, 0.0]
+        elif dimension == 2:
+            self.platesAnglesDic[3] = [0.0, 0.0]
+            self.platesAnglesDic[4] = [0.0, 0.0]
+
         # Hadamard corrections are considered at qubits 1 and 4 for the Linear Cluster L4
         if prename == "Linear Cluster":
+            print(
+                mssg + "Absorbing H gate for qubits 1 and 4 needed for L4 cluster state.")
             self.platesAnglesDic[1] = had_corr(self.platesAnglesDic[1])
             self.platesAnglesDic[4] = had_corr(self.platesAnglesDic[4])
 
-        print(self.platesAnglesDic)
+        print(mssg + "Full experiment plates angles dictionary: " +
+              str(self.platesAnglesDic))
 
     def setPlatesAngles(self):
         '''
         move the plates to the angles calculated in getPlatesAngles
         '''
+        mssg = 'Experiment.setPlatesAngles :: '
+        print(mssg + 'using platesAnglesDic (loaded with getPlatesAngles) to set waveplates angles')
+
         p_array1 = PlatesArray(1)
 
         p_array1.init()
 
         for path in range(5):
+            print(mssg + 'Setting up path ' + str(path) +
+                  ' at angles ' + str(self.platesAnglesDic[path]))
             p_array1.setPath(path, self.platesAnglesDic[path])
 
         p_array1.fina()
-
-    # This section applied when the TT was connected to this PC
-    # def rawMeasure(self):
-    #     '''
-    #     usethe timetager
-    #     '''
-    #     timetagger = Timetagger()
-
-    #     timetagger.countrates()
-
-    #     timetagger.circuitOutput(self.circuitLib)
+        print(mssg + 'Done. All waveplates aligned.')
--- ControlRunExperiment.py
+++ ControlRunExperiment.py
@@ -4,7 +4,13 @@ from utilFunc import formatDic
 
 
 class RunExperiment:
-    """"""
+    '''
+    Class for running the full experiment.
+    This imples:
+        Setting waveplates using Experiment class
+        Sending the instruction to the timetagger PC to measure coincidences
+        Retrieve the result
+    '''
 
     def __init__(self, expeDicI: dict, expId):
         '''
@@ -16,19 +22,24 @@ class RunExperiment:
 
         '''
 
+        mssg = 'Experiment.RunExperiment :: '
+
+        print(mssg + "Preparing to implement experiment with circuit ID: " + str(expId))
         expeDic = formatDic(expeDicI)
 
         self.exp = Experiment(expeDic)
+        print(mssg + "Experiment initialized.")
 
         # getPlatesAngles calculates the angles to set the waveplates as a funtion of the circuit and measurement paramenters
         self.exp.getPlatesAngles()
 
-        print("--------------------------------------------------A")
         # setPlatesAngles rotates each waveplate to its corresponding angle
         self.exp.setPlatesAngles()
-        print("--------------------------------------------------B")
+        print(mssg + "Experiment hardware on place. Ready to measure coincidences.")
 
         self.rex = RemoteExec(expeDic, expId)
-        print("--------------------------------------------------C")
+        print(mssg + "Remote execution completed. Timetagger results retried.")
+
         self.results = self.rex.res_json
+        print(mssg + 'Experiment results:')
         print(self.results)
