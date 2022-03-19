93aa25f9b82c17144bdc10dfa54f5bbee33af0c0
CDL-Univie <77630497+CDL-Univie@users.noreply.github.com>
2022-03-19 21:49:37 +0100

    use on_click for all buttons

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from contextlib import contextmanager, redirect_stdout
+from io import StringIO
 import streamlit as st
+from streamlit.script_run_context import add_script_run_ctx
 import base64
 import textwrap
 from CalibrationTools import Calibration
@@ -24,6 +27,75 @@ def render_svg_setup():
 
     render_svg(svg)
     st.write('Experimental setup')
+
+
+@contextmanager
+def st_capture(output_func):
+    with StringIO() as stdout, redirect_stdout(stdout):
+        old_write = stdout.write
+
+        def new_write(string):
+            ret = old_write(string)
+            output_func(stdout.getvalue())
+            return ret
+
+        stdout.write = new_write
+        yield
+
+
+def init_plates():  # Initialize connection to plates array. Store init flag pa_init
+
+    # if 'code_display' not in st.session_state:
+    #     st.session_state['code_display'] = st.empty()
+
+    if 'pa' not in st.session_state:
+
+        # output = st.empty()
+
+        # with st_capture(output.code):
+        with st_capture(st.session_state['code_display'].code):
+            st.session_state['pa'] = PlatesArray(1)
+
+            st.session_state['pa_init'] = st.session_state['pa'].init()
+
+
+def fina_plates():  # Close connection to plates array. Store init flag pa_init
+
+    # output = st.empty()
+    # st.session_state['code_display'] = st.empty()
+
+    st.session_state['pa_init'] = False
+
+    # with st_capture(output.code):
+    with st_capture(st.session_state['code_display'].code):
+        st.session_state['pa'].fina()
+
+
+def move_all_plates():  # Close connection to plates array. Store init flag pa_init
+
+    # st.session_state['code_display'] = st.empty()
+
+    # with st_capture(output.code):
+    with st_capture(st.session_state['code_display'].code):
+
+        if st.session_state['pa_init']:
+
+            st.session_state['pa'].setPlate(-1, 0, ang_Hm1)
+            st.session_state['pa'].setPlate(-2, 0, ang_Hm2)
+            st.session_state['pa'].setPlate(-3, 0, ang_Hm3)
+            st.session_state['pa'].setPlate(-4, 0, ang_Hm4)
+
+            st.session_state['pa'].setPlate(0, 0, ang_H0)
+
+            st.session_state['pa'].setPlate(1, 0, ang_Q1)
+            st.session_state['pa'].setPlate(2, 0, ang_Q2)
+            st.session_state['pa'].setPlate(3, 0, ang_Q3)
+            st.session_state['pa'].setPlate(4, 0, ang_Q4)
+            st.session_state['pa'].setPlate(1, 1, ang_H1)
+
+            st.session_state['pa'].setPlate(2, 1, ang_H2)
+            st.session_state['pa'].setPlate(3, 1, ang_H3)
+            st.session_state['pa'].setPlate(4, 1, ang_H4)
 
 
 if __name__ == '__main__':
@@ -57,18 +129,34 @@ if __name__ == '__main__':
     # General mode
     if mode_selectbox == "General":
 
-        st.write('General')
-
+        # Initialization of plastes rotation mounts
         init_plates = st.sidebar.button(
-            'Initialize', disabled=st.session_state['pa_init'])
+            'Connect to plates', disabled=st.session_state['pa_init'], on_click=init_plates)
 
-        if init_plates:
+        # Description
+        if st.session_state['pa_init']:
+            conection_message = 'Plates connected'
+        else:
+            conection_message = 'Plates disconnected'
 
-            if 'pa' not in st.session_state:
+        col1_gen_des, col2_gen_des = st.columns(2)
 
-                st.session_state['pa'] = PlatesArray(1)
-                # Initialize connection to plates array. Store init flag pa_init
-                st.session_state['pa_init'] = st.session_state['pa'].init()
+        with col1_gen_des:
+
+            st.write('General operation mode - ' + conection_message)
+
+        with col2_gen_des:
+
+            # Close connection
+            close = st.button(
+                'Close connection', disabled=not st.session_state['pa_init'], on_click=fina_plates)
+
+            # if close:
+
+            #     st.session_state['pa'].fina()
+
+            #     st.session_state['pa_init'] = False
+            #     print(st.session_state['pa_init'])
 
         # setup svg
         render_svg_setup()
@@ -103,37 +191,14 @@ if __name__ == '__main__':
 
         # Move all plates control
         move_all = st.button(
-            'Move all', disabled=not st.session_state['pa_init'])
-
-        if st.session_state['pa_init'] and move_all:
-
-            st.session_state['pa'].setPlate(-1, 0, ang_Hm1)
-            st.session_state['pa'].setPlate(-2, 0, ang_Hm2)
-            st.session_state['pa'].setPlate(-3, 0, ang_Hm3)
-            st.session_state['pa'].setPlate(-4, 0, ang_Hm4)
-
-            st.session_state['pa'].setPlate(0, 0, ang_H0)
-
-            st.session_state['pa'].setPlate(1, 0, ang_Q1)
-            st.session_state['pa'].setPlate(2, 0, ang_Q2)
-            st.session_state['pa'].setPlate(3, 0, ang_Q3)
-            st.session_state['pa'].setPlate(4, 0, ang_Q4)
-            st.session_state['pa'].setPlate(1, 1, ang_H1)
-
-            st.session_state['pa'].setPlate(2, 1, ang_H2)
-            st.session_state['pa'].setPlate(3, 1, ang_H3)
-            st.session_state['pa'].setPlate(4, 1, ang_H4)
-
-        # Close connection
-        close = st.button('Close connection',
-                          disabled=not st.session_state['pa_init'])
+            'Move all', disabled=not st.session_state['pa_init'], on_click=move_all_plates)
 
         if close:
 
-            st.session_state['pa'].fina()
+            st.info('Plates disconnected - Reload page or change operation mode')
 
-            st.session_state['pa_init'] = False
-            print(st.session_state['pa_init'])
+        if 'code_display' not in st.session_state:
+            st.session_state['code_display'] = st.empty()
 
     # Calibration Mode
     elif mode_selectbox == "Calibration":
