06a9fec56742e22b7f56a58da39d4aa797d79140
Felix Zilk <zilk.felix@gmail.com>
2022-04-27 17:41:57 +0200

    add single and two-qubit circuits to simulation

--- qutip_simulation/circuits.py
+++ qutip_simulation/circuits.py
@@ -11,6 +11,37 @@ import math
 import cmath
 
 
+def measure_one(state, theta1, phi1):
+    state0 = basis(2, 0)
+    state1 = basis(2, 1)
+
+    z1 = complex(0, phi1)
+
+    E1 = (
+        math.cos(theta1 / 2) * state0 + cmath.exp(z1) *
+        math.sin(theta1 / 2) * state1
+    ).unit()
+    E1_orth = (
+        -math.cos(theta1 / 2) * state1 + cmath.exp(-z1) *
+        math.sin(theta1 / 2) * state0
+    ).unit()
+
+    matrix = (ket2dm(E1) - ket2dm(E1_orth))
+
+    results = {"0": 0, "1": 0}
+
+    for _ in range(1000):
+        value, new_state = measure(state, matrix)
+        if value == 1:
+            results["0"] += 1
+        elif value == -1:
+            results["1"] += 1
+        else:
+            print("Unexpected error.")
+
+    return results
+
+
 def measure_two(state, theta1, phi1, theta2, phi2):
     state0 = basis(2, 0)
     state1 = basis(2, 1)
@@ -66,6 +97,102 @@ def measure_two(state, theta1, phi1, theta2, phi2):
     return results
 
 
+def circuit_5(alpha, theta1, phi1):
+    qc = QubitCircuit(N=1)
+    qc.add_gate("SNOT", targets=[0])
+    qc.add_gate("RZ", targets=[0], arg_value=alpha)
+    qc.add_gate("SNOT", targets=[0])
+
+    init_state = basis(2, 0)
+    print(init_state)
+    result = qc.run(state=init_state)
+
+    results = measure_one(state=result, theta1=theta1,
+                          phi1=phi1)
+    return results
+
+
+def circuit_6(theta1, phi1, theta2, phi2):
+    qc = QubitCircuit(N=2)
+    qc.add_gate("SNOT", targets=[0])
+    qc.add_gate("SNOT", targets=[1])
+    qc.add_gate("CSIGN", targets=[1], controls=[0])
+
+    init_state = tensor(basis(2, 0), basis(2, 0))
+    print(init_state)
+    result = qc.run(state=init_state)
+
+    results = measure_two(state=result, theta1=theta1,
+                          phi1=phi1, theta2=theta2, phi2=phi2)
+    return results
+
+
+def circuit_7(alpha, beta, theta1, phi1):
+    qc = QubitCircuit(N=1)
+    qc.add_gate("SNOT", targets=[0])
+    qc.add_gate("RZ", targets=[0], arg_value=alpha)
+    qc.add_gate("RX", targets=[0], arg_value=beta)
+
+    init_state = basis(2, 0)
+    print(init_state)
+    result = qc.run(state=init_state)
+
+    results = measure_one(state=result, theta1=theta1,
+                          phi1=phi1)
+    return results
+
+
+def circuit_8(alpha, theta1, phi1, theta2, phi2):
+    qc = QubitCircuit(N=2)
+    qc.add_gate("SNOT", targets=[0])
+    qc.add_gate("SNOT", targets=[1])
+    qc.add_gate("RZ", targets=[1], arg_value=alpha)
+    qc.add_gate("SNOT", targets=[1])
+    qc.add_gate("CSIGN", targets=[1], controls=[0])
+
+    init_state = tensor(basis(2, 0), basis(2, 0))
+    print(init_state)
+    result = qc.run(state=init_state)
+
+    results = measure_two(state=result, theta1=theta1,
+                          phi1=phi1, theta2=theta2, phi2=phi2)
+    return results
+
+
+def circuit_9(alpha, theta1, phi1, theta2, phi2):
+    qc = QubitCircuit(N=2)
+    qc.add_gate("SNOT", targets=[0])
+    qc.add_gate("SNOT", targets=[1])
+    qc.add_gate("CSIGN", targets=[1], controls=[0])
+    qc.add_gate("RZ", targets=[1], arg_value=alpha)
+    qc.add_gate("SNOT", targets=[1])
+
+    init_state = tensor(basis(2, 0), basis(2, 0))
+    print(init_state)
+    result = qc.run(state=init_state)
+
+    results = measure_two(state=result, theta1=theta1,
+                          phi1=phi1, theta2=theta2, phi2=phi2)
+    return results
+
+
+def circuit_11(alpha, beta, gamma, theta1, phi1):
+    qc = QubitCircuit(N=1)
+    qc.add_gate("SNOT", targets=[0])
+    qc.add_gate("RZ", targets=[0], arg_value=alpha)
+    qc.add_gate("RX", targets=[0], arg_value=beta)
+    qc.add_gate("RZ", targets=[0], arg_value=gamma)
+    qc.add_gate("SNOT", targets=[0])
+
+    init_state = basis(2, 0)
+    print(init_state)
+    result = qc.run(state=init_state)
+
+    results = measure_one(state=result, theta1=theta1,
+                          phi1=phi1)
+    return results
+
+
 def circuit_12(alpha, beta, theta1, phi1, theta2, phi2):
     qc = QubitCircuit(N=2)
     qc.add_gate("SNOT", targets=[0])
@@ -116,6 +243,25 @@ def circuit_14(alpha, beta, theta1, phi1, theta2, phi2):
 
     init_state = tensor(basis(2, 0), basis(2, 0))
 
+    result = qc.run(state=init_state)
+
+    results = measure_two(state=result, theta1=theta1,
+                          phi1=phi1, theta2=theta2, phi2=phi2)
+    return results
+
+
+def circuit_19(alpha, beta, theta1, phi1, theta2, phi2):
+    qc = QubitCircuit(N=2)
+    qc.add_gate("SNOT", targets=[0])
+    qc.add_gate("SNOT", targets=[1])
+    qc.add_gate("RZ", targets=[0], arg_value=alpha)
+    qc.add_gate("SNOT", targets=[0])
+    qc.add_gate("CSIGN", targets=[1], controls=[0])
+    qc.add_gate("RZ", targets=[0], arg_value=beta)
+    qc.add_gate("SNOT", targets=[0])
+
+    init_state = tensor(basis(2, 0), basis(2, 0))
+    print(init_state)
     result = qc.run(state=init_state)
 
     results = measure_two(state=result, theta1=theta1,
--- qutip_simulation/execute.py
+++ qutip_simulation/execute.py
@@ -1,5 +1,4 @@
 from qutip_simulation.circuits import *
-from qutip_simulation.test import test
 
 
 def execute(circuitId, ComputeSettings):
@@ -7,29 +6,81 @@ def execute(circuitId, ComputeSettings):
     angles are retrieved as sting type 'str' and need conversion to float
     """
     id = circuitId
-    if id in [12, 13, 14]:
-        alpha_str = ComputeSettings["qubitComputing"]["circuitAngles"][0]["circuitAngleValue"]
-        beta_str = ComputeSettings["qubitComputing"]["circuitAngles"][1]["circuitAngleValue"]
+    # if circuit encodes 1 logical qubit we always retrieve
+    # theta1, phi1
+    if id in [5, 7, 11]:
+        theta1_str = ComputeSettings["encodedQubitMeasurements"][0]["theta"]
+        phi1_str = ComputeSettings["encodedQubitMeasurements"][0]["phi"]
+        theta1 = float(theta1_str)
+        phi1 = float(phi1_str)
 
+        if id == 5:
+            alpha_str = ComputeSettings["qubitComputing"]["circuitAngles"][0]["circuitAngleValue"]
+            alpha = float(alpha_str)
+            res = circuit_5(alpha=alpha, theta1=theta1,
+                            phi1=phi1, theta2=theta2, phi2=phi2)
+
+        if id == 7:
+            alpha_str = ComputeSettings["qubitComputing"]["circuitAngles"][0]["circuitAngleValue"]
+            beta_str = ComputeSettings["qubitComputing"]["circuitAngles"][1]["circuitAngleValue"]
+            alpha = float(alpha_str)
+            beta = float(beta_str)
+            res = circuit_7(alpha=alpha, beta=beta, theta1=theta1,
+                            phi1=phi1, theta2=theta2, phi2=phi2)
+
+        if id == 11:
+            alpha_str = ComputeSettings["qubitComputing"]["circuitAngles"][0]["circuitAngleValue"]
+            beta_str = ComputeSettings["qubitComputing"]["circuitAngles"][1]["circuitAngleValue"]
+            gamma_str = ComputeSettings["qubitComputing"]["circuitAngles"][2]["circuitAngleValue"]
+            alpha = float(alpha_str)
+            beta = float(beta_str)
+            gamma = float(gamma_str)
+            res = circuit_11(alpha=alpha, beta=beta, gamma=gamma, theta1=theta1,
+                             phi1=phi1, theta2=theta2, phi2=phi2)
+
+    # if circuit encodes 2 logical qubits we always retrieve
+    # theta1, phi1, theta2, phi2
+    if id in [6, 8, 9, 12, 13, 14, 19]:
         theta1_str = ComputeSettings["encodedQubitMeasurements"][0]["theta"]
         phi1_str = ComputeSettings["encodedQubitMeasurements"][0]["phi"]
         theta2_str = ComputeSettings["encodedQubitMeasurements"][1]["theta"]
         phi2_str = ComputeSettings["encodedQubitMeasurements"][1]["phi"]
-
-        alpha = float(alpha_str)
-        beta = float(beta_str)
         theta1 = float(theta1_str)
         phi1 = float(phi1_str)
         theta2 = float(theta2_str)
         phi2 = float(phi2_str)
 
-        if id == 12:
-            res = circuit_12(alpha=alpha, beta=beta, theta1=theta1,
-                             phi1=phi1, theta2=theta2, phi2=phi2)
-        if id == 13:
-            res = circuit_13(alpha=alpha, beta=beta, theta1=theta1,
-                             phi1=phi1, theta2=theta2, phi2=phi2)
-        if id == 14:
-            res = circuit_14(alpha=alpha, beta=beta, theta1=theta1,
-                             phi1=phi1, theta2=theta2, phi2=phi2)
-        return res
+        if id == 6:
+            res = circuit_6(theta1=theta1,
+                            phi1=phi1, theta2=theta2, phi2=phi2)
+
+        if id in [8, 9]:
+            alpha_str = ComputeSettings["qubitComputing"]["circuitAngles"][0]["circuitAngleValue"]
+            alpha = float(alpha_str)
+
+            if id == 8:
+                res = circuit_8(alpha=alpha, theta1=theta1,
+                                phi1=phi1, theta2=theta2, phi2=phi2)
+            if id == 9:
+                res = circuit_9(alpha=alpha, theta1=theta1,
+                                phi1=phi1, theta2=theta2, phi2=phi2)
+
+        if id in [12, 13, 14, 19]:
+            alpha_str = ComputeSettings["qubitComputing"]["circuitAngles"][0]["circuitAngleValue"]
+            beta_str = ComputeSettings["qubitComputing"]["circuitAngles"][1]["circuitAngleValue"]
+            alpha = float(alpha_str)
+            beta = float(beta_str)
+
+            if id == 12:
+                res = circuit_12(alpha=alpha, beta=beta, theta1=theta1,
+                                 phi1=phi1, theta2=theta2, phi2=phi2)
+            if id == 13:
+                res = circuit_13(alpha=alpha, beta=beta, theta1=theta1,
+                                 phi1=phi1, theta2=theta2, phi2=phi2)
+            if id == 14:
+                res = circuit_14(alpha=alpha, beta=beta, theta1=theta1,
+                                 phi1=phi1, theta2=theta2, phi2=phi2)
+            if id == 19:
+                res = circuit_19(alpha=alpha, beta=beta, theta1=theta1,
+                                 phi1=phi1, theta2=theta2, phi2=phi2)
+    return res
